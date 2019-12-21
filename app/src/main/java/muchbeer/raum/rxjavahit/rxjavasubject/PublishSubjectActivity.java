911116e7955cd5941e2e62ba23b065fa9c8c85c6
muchbeer <gman2them@gmail.com>
2019-12-21 22:23:44 +0300

    Implement AsyncSubjectActivity

---
+++ app/src/main/java/muchbeer/raum/rxjavahit/rxjavasubject/PublishSubjectActivity.java
@@ -0,0 +1,23 @@
+package muchbeer.raum.rxjavahit.rxjavasubject;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import muchbeer.raum.rxjavahit.R;
+
+public class PublishSubjectActivity extends AppCompatActivity {
+
+    private final static String TAG = PublishSubjectActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       // asyncSubjectDemo1();
+
+    }
+
+}
