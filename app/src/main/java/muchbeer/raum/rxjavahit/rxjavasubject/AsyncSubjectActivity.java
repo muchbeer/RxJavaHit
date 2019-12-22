8c2f14cb7206f264e2d8c089df21a76887da9244
muchbeer <gman2them@gmail.com>
2019-12-22 10:16:18 +0300

    Implement AsyncSubjet, BehaviorSubject, PublishSubject and ReplaySubject

--- app/src/main/java/muchbeer/raum/rxjavahit/rxjavasubject/AsyncSubjectActivity.java
+++ app/src/main/java/muchbeer/raum/rxjavahit/rxjavasubject/AsyncSubjectActivity.java
@@ -12,6 +12,9 @@ import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 import io.reactivex.subjects.AsyncSubject;
+import io.reactivex.subjects.BehaviorSubject;
+import io.reactivex.subjects.PublishSubject;
+import io.reactivex.subjects.ReplaySubject;
 import muchbeer.raum.rxjavahit.R;
 
 public class AsyncSubjectActivity extends AppCompatActivity {
@@ -22,9 +25,18 @@ public class AsyncSubjectActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        asyncSubjectDemo1();
+        //asyncSubjectDemo1();
+        //asyncSubjectDemo2();
+        //behaviorSubjectDemo1();
+        //behaviorSubjectDemo2();
+        //publishSubjectDemo1();
+        //publishSubjectDemo2();
+        //replaySubjectDemo1();
+        replaySubjectDemo2();
 
     }
+
+    //emit the last item of the observable
     void asyncSubjectDemo1() {
 
         Observable<String> observable = Observable.just("JAVA", "KOTLIN", "XML", "JSON")
@@ -38,6 +50,144 @@ public class AsyncSubjectActivity extends AppCompatActivity {
         asyncSubject.subscribe(getFirstObserver());
         asyncSubject.subscribe(getSecondObserver());
         asyncSubject.subscribe(getThirdObserver());
+    }
+
+
+    void asyncSubjectDemo2() {
+
+
+        AsyncSubject<String> asyncSubject = AsyncSubject.create();
+
+        asyncSubject.subscribe(getFirstObserver());
+
+        asyncSubject.onNext("JAVA");
+        asyncSubject.onNext("KOTLIN");
+        asyncSubject.onNext("XML");
+
+        asyncSubject.subscribe(getSecondObserver());
+
+        asyncSubject.onNext("JSON");
+        asyncSubject.onComplete();
+
+        asyncSubject.subscribe(getThirdObserver());
+
+
+    }
+
+//subject Emit recently emited item and all the subsequenty item
+    void behaviorSubjectDemo1() {
+        Observable<String> observable = Observable.just("JAVA", "KOTLIN", "XML", "JSON")
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+
+        BehaviorSubject<String> behaviorSubject = BehaviorSubject.create();
+
+        observable.subscribe(behaviorSubject);
+
+        behaviorSubject.subscribe(getFirstObserver());
+        behaviorSubject.subscribe(getSecondObserver());
+        behaviorSubject.subscribe(getThirdObserver());
+
+
+    }
+
+
+    void behaviorSubjectDemo2() {
+
+
+        BehaviorSubject<String> behaviorSubject = BehaviorSubject.create();
+
+        behaviorSubject.subscribe(getFirstObserver());
+
+        behaviorSubject.onNext("JAVA");
+        behaviorSubject.onNext("KOTLIN");
+        behaviorSubject.onNext("XML");
+
+        behaviorSubject.subscribe(getSecondObserver());
+
+        behaviorSubject.onNext("JSON");
+        behaviorSubject.onComplete();
+
+        behaviorSubject.subscribe(getThirdObserver());
+
+
+    }
+
+//emit to all that are subscribe to it
+    void publishSubjectDemo1() {
+
+        Observable<String> observable = Observable.just("JAVA", "KOTLIN", "XML", "JSON")
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+
+        PublishSubject<String> publishSubject = PublishSubject.create();
+
+        observable.subscribe(publishSubject);
+
+        publishSubject.subscribe(getFirstObserver());
+        publishSubject.subscribe(getSecondObserver());
+        publishSubject.subscribe(getThirdObserver());
+
+
+    }
+
+    void publishSubjectDemo2() {
+
+
+        PublishSubject<String> publishSubject = PublishSubject.create();
+
+        publishSubject.subscribe(getFirstObserver());
+
+        publishSubject.onNext("JAVA");
+        publishSubject.onNext("KOTLIN");
+        publishSubject.onNext("XML");
+
+        publishSubject.subscribe(getSecondObserver());
+
+        publishSubject.onNext("JSON");
+        publishSubject.onComplete();
+
+        publishSubject.subscribe(getThirdObserver());
+
+
+    }
+
+    //Emit all items that were item by the source
+    void replaySubjectDemo1() {
+
+        Observable<String> observable = Observable.just("JAVA", "KOTLIN", "XML", "JSON")
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+
+        ReplaySubject<String> replaySubject = ReplaySubject.create();
+
+        observable.subscribe(replaySubject);
+
+        replaySubject.subscribe(getFirstObserver());
+        replaySubject.subscribe(getSecondObserver());
+        replaySubject.subscribe(getThirdObserver());
+
+
+    }
+
+
+    void replaySubjectDemo2() {
+
+
+        ReplaySubject<String> replaySubject = ReplaySubject.create();
+
+        replaySubject.subscribe(getFirstObserver());
+
+        replaySubject.onNext("JAVA");
+        replaySubject.onNext("KOTLIN");
+        replaySubject.onNext("XML");
+
+        replaySubject.subscribe(getSecondObserver());
+
+        replaySubject.onNext("JSON");
+        replaySubject.onComplete();
+
+        replaySubject.subscribe(getThirdObserver());
 
 
     }
@@ -92,13 +242,11 @@ public class AsyncSubjectActivity extends AppCompatActivity {
 
             @Override
             public void onError(Throwable e) {
-
                 Log.i(TAG, " Second Observer onError ");
             }
 
             @Override
             public void onComplete() {
-
                 Log.i(TAG, " Second Observer onComplete ");
 
             }
@@ -112,8 +260,6 @@ public class AsyncSubjectActivity extends AppCompatActivity {
         Observer<String> observer = new Observer<String>() {
             @Override
             public void onSubscribe(Disposable d) {
-
-
                 Log.i(TAG, " Third Observer onSubscribe ");
             }
 
